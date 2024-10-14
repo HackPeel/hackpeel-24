418e2000f9596068a7e6beef5d08054045a9731c
VedantGithub123 <vedantnehete@gmail.com>
2024-10-14 11:24:40 -0400

    Started Footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function removeTaskbar()
 {
-    if (screen.width/screen.height < 1/1.7){
-        var element = document.getElementById("taskbar-orgnaizers");
+    if (screen.width/screen.height < 1.0/1.7){
+        var element = document.getElementById("taskbar-organizers");
+        element.parentNode.removeChild(element);
+        element = document.getElementById("taskbar-faq");
         element.parentNode.removeChild(element);
     }
 }
